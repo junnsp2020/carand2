cfd6fd32b74719f970b5f193adfa538acef8ff6c
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-06-02 16:08:12 +0900

    [Add] Users(showページを記述した)

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -4,4 +4,9 @@ class Users::UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :prefecture_code, :address_city, :address_street, :address_building, :phone_number, :bank_name, :account_type, :branch_code, :account_number, :account_last_name_kana, :account_first_name_kana, :buyer_id, :seller_id, :balance, :request_amount, :image, :nickname, :introduction)
+  end
 end
