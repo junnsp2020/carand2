c15201de8cc366635eb830b656e5275564b97249
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-06-04 00:07:34 +0900

    [Add] Categories((コントローラー設定)

--- app/controllers/administers/categories_controller.rb
+++ app/controllers/administers/categories_controller.rb
@@ -1,2 +1,33 @@
 class Administers::CategoriesController < ApplicationController
+  def index
+  	@categories = Category.new
+  	@categories = Category.all
+  end
+
+  def create
+  	@category = Category.new(category_params)
+  	if @category.save
+  	  redirect_to administers_categories_path
+  	else
+  	  render :index
+  	end
+  end
+
+  def edit
+  	@category = Category.find(params[:id])
+  end
+
+  def update
+  	@category = Category.find(params[:id])
+  	if @category.update(category_params)
+  	  redirect_to administers_categories_path
+  	else
+  	  render :edit
+  	end
+  end
+
+  private
+  def category_params
+  	params.require(:category).permit(:name, :status)
+  end
 end
