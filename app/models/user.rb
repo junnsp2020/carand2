e54174e3f03f6eb335fda556a5f64456d3d790f3
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-06-28 14:32:54 +0900

    [Add] Users(モデル/バリデーションを追加)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
   has_many :passive_tradings, class_name: "Trading", foreign_key: :seller_id
   has_many :sellers, through: :passive_relationships, source: :buyer
   has_many :trading_messages
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :postcode, presence: true
+  validates :prefecture_code, presence: true
+  validates :address_city, presence: true
+  validates :address_street, presence: true
+  validates :phone_number, presence: true
 end
