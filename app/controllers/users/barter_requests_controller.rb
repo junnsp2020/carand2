1f6f52c23969c52aa37b6eec80736903ac05db8d
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-06-30 00:29:41 +0900

    [Add] BarterRequests(requestedページを作成)

--- app/controllers/users/barter_requests_controller.rb
+++ app/controllers/users/barter_requests_controller.rb
@@ -30,6 +30,29 @@ class Users::BarterRequestsController < ApplicationController
     		render :new
     	end
     end
+
+    def update
+        @barter_request = BarterRequest.find(params[:id])
+        @product = Product.find(params[:product_id])
+        if @product.notice == false
+            @product.notice = true
+            @product.save
+        end
+        @barter_request.update(barter_request_params)
+        if @barter_request.propriety == "許可する"
+            @barter_request.notice = true
+            @barter_request.save
+            redirect_to request.referer
+        else
+            redirect_to request.referer
+        end
+    end
+
+    def requested
+        @product = Product.where(user_id: current_user.id)
+        @barter_requests = BarterRequest.where(product_id: @product)
+    end
+
     private
     def barter_request_params
     	params.require(:barter_request).permit(:product_id, :user_id, :name, :image, :introduction, :comment, :product_condition, :propriety, :buyer_id, :seller_id, :notice)
